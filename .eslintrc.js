ff970ac9b34762fe25751994d537f2bda1e709f5
Alexandrea Defreitas <alexandreadef@gmail.com>
2017-03-02 10:46:42 -0500

    Installs test suite

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
         "import"
     ],
     'rules': {
-      'no-underscore-dangle': 0,
+      'no-underscore-dangle': 0, // throws an error with mongo's _id prop
+      'no-unused-expressions': 0, // was throwing errors for expect()
+    },
+    'env': {
+      'node': true,
+      'mocha': true,
     },
 };
